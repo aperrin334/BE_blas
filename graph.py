491a1ca7f21449745389f527b479e3dc2b5f9f5b
nlkris <cnguyenluc@gmail.com>
2025-10-10 21:10:27 +0200

    Graph BLAS1 et BLAS2 + Mise en page des graphs

--- graph.py
+++ graph.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import csv
 import numpy as np
 
-
 data1 = np.genfromtxt('timings_add.csv', delimiter='		', skip_header = 1)
 data2 = np.genfromtxt('timings_add-inorder.csv', delimiter='		', skip_header = 1)
+datablas1 = np.genfromtxt('timings_BLAS1.csv', delimiter='		', skip_header = 1)
+datablas2 = np.genfromtxt('timings_BLAS2.csv', delimiter='		', skip_header = 1)
+
 
 x1 = data1[:,0]
 y1 = data1[:,1]
@@ -14,9 +16,56 @@ x2 = data2[:,0]
 y2= data2[:,1]
 z2 = data2[:,2]
 
-plt.plot(x1,y1)
-plt.plot(x2,y2)
+xblas1 = datablas1[:,0]
+yblas1= datablas1[:,1]
+zblas1 = datablas1[:,2]
+
+xblas2 = datablas2[:,0]
+yblas2= datablas2[:,1]
+zblas2 = datablas2[:,2]
+
+fig1, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+# Execution time
+ax1.plot(x1,y1, label='inhous_add')
+ax1.plot(x2, y2,label='inhouse_add_reorder')
+ax1.set_title("Evolution du temps moyen d'execution USE_ADD")
+ax1.set_xlabel("Dimension")
+ax1.set_ylabel("time (s)")
+
+# Calculation speed
+ax2.plot(x1, z1, label='inhous_add')
+ax2.plot(x2, z2, label='inhous_add_reorder')
+ax2.set_title("Evolution de la rapidité de calcul (Gflop/S) USE_ADD")
+ax2.set_xlabel("Dimension")
+ax2.set_ylabel("Gflops/s")
+plt.tight_layout()
 plt.show()
-plt.plot(x1,z1)
-plt.plot(x2,z2)
+
+fig2, (ax3, ax4) = plt.subplots(1, 2, figsize=(12, 5))
+
+ax3.plot(xblas1,yblas1, color='m')
+ax3.set_title("Evolution du temps moyen d'execution \n BLAS1")
+ax3.set_xlabel("Dimension")
+ax3.set_ylabel("time (s)")
+
+ax4.plot(xblas1,zblas1,color='m')
+ax4.set_title("Evolution de la rapidité de calcul (Gflop/S) \n BLAS1")
+ax4.set_xlabel("Dimension")
+ax4.set_ylabel("Gflops/s")
+plt.tight_layout()
+plt.show()
+
+fig3, (ax5, ax6) = plt.subplots(1, 2, figsize=(12, 5))
+
+ax5.plot(xblas2,yblas2,color='g')
+ax5.set_title("Evolution du temps moyen d'execution BLAS2 ")
+ax5.set_xlabel("Dimension")
+ax5.set_ylabel("time (s)")
+
+ax6.plot(xblas2,zblas2,color='g')
+ax6.set_title("Evolution de la rapidité de calcul (Gflop/S) BLAS2")
+ax6.set_xlabel("Dimension")
+ax6.set_ylabel("Gflops/s")
+plt.tight_layout()
 plt.show()
