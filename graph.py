18ef0d22a6a5db75ef1f02874dd31dbe59cda9d0
aperrin334 <aliperrin14@gmail.com>
2025-10-17 16:42:07 +0200

    graphes para

--- graph.py
+++ graph.py
@@ -10,6 +10,19 @@ datablas3 = np.genfromtxt('timings_BLAS3.csv', delimiter='		', skip_header = 1)
 datablas1ob = np.genfromtxt('timings_BLAS1_openblas.csv', delimiter='		', skip_header = 1)
 datablas2ob = np.genfromtxt('timings_BLAS2_openblas.csv', delimiter='		', skip_header = 1)
 datablas3ob = np.genfromtxt('timings_BLAS3_openblas.csv', delimiter='		', skip_header = 1)
+datamuld2 = np.genfromtxt('timings_para_d2.csv', delimiter='		', skip_header = 1)
+datamuld4 = np.genfromtxt('timings_para_d4.csv', delimiter='		', skip_header = 1)
+datamuld6 = np.genfromtxt('timings_para_d6.csv', delimiter='		', skip_header = 1)
+datamuld8 = np.genfromtxt('timings_para_d8.csv', delimiter='		', skip_header = 1)
+datamuld10 = np.genfromtxt('timings_para_d10.csv', delimiter='		', skip_header = 1)
+datamuld12 = np.genfromtxt('timings_para_d12.csv', delimiter='		', skip_header = 1)
+datamuls2 = np.genfromtxt('timings_para_s2.csv', delimiter='		', skip_header = 1)
+datamuls4 = np.genfromtxt('timings_para_s4.csv', delimiter='		', skip_header = 1)
+datamuls6 = np.genfromtxt('timings_para_s6.csv', delimiter='		', skip_header = 1)
+datamuls8 = np.genfromtxt('timings_para_s8.csv', delimiter='		', skip_header = 1)
+datamuls10 = np.genfromtxt('timings_para_s10.csv', delimiter='		', skip_header = 1)
+datamuls12 = np.genfromtxt('timings_para_s12.csv', delimiter='		', skip_header = 1)
+
 
 x1 = data1[:,0]
 y1 = data1[:,1]
@@ -42,6 +55,54 @@ zblas2ob = datablas2ob[:,2]
 xblas3ob = datablas3ob[:,0]
 yblas3ob = datablas3ob[:,1]
 zblas3ob = datablas3ob[:,2]
+
+xd2 = datamuld2[:,0]
+yd2 = datamuld2[:,1]
+zd2 = datamuld2[:,2]
+
+xd4 = datamuld4[:,0]
+yd4 = datamuld4[:,1]
+zd4 = datamuld4[:,2]
+
+xd6 = datamuld6[:,0]
+yd6 = datamuld6[:,1]
+zd6 = datamuld6[:,2]
+
+xd8 = datamuld8[:,0]
+yd8 = datamuld8[:,1]
+zd8 = datamuld8[:,2]
+
+xd10 = datamuld10[:,0]
+yd10 = datamuld10[:,1]
+zd10 = datamuld10[:,2]
+
+xd12 = datamuld12[:,0]
+yd12 = datamuld12[:,1]
+zd12 = datamuld12[:,2]
+
+xs2 = datamuls2[:,0]
+ys2 = datamuls2[:,1]
+zs2 = datamuls2[:,2]
+
+xs4 = datamuls4[:,0]
+ys4 = datamuls4[:,1]
+zs4 = datamuls4[:,2]
+
+xs6 = datamuls6[:,0]
+ys6 = datamuls6[:,1]
+zs6 = datamuls6[:,2]
+
+xs8 = datamuls8[:,0]
+ys8 = datamuls8[:,1]
+zs8 = datamuls8[:,2]
+
+xs10 = datamuls10[:,0]
+ys10 = datamuls10[:,1]
+zs10 = datamuls10[:,2]
+
+xs12 = datamuls12[:,0]
+ys12 = datamuls12[:,1]
+zs12 = datamuls12[:,2]
 
 #Figure USE_ADD
 fig1, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
@@ -98,6 +159,32 @@ ax6.plot(xblas3ob,zblas3ob,color='r', label='BLAS3')
 ax6.set_title("Evolution de la rapidité de calcul (Gflop/S)")
 ax6.set_xlabel("Dimension")
 ax6.set_ylabel("Gflops/s")
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+#Figure MUL avec optimisation parallelisation dynamique
+fig2, (ax7, ax8) = plt.subplots(1, 2, figsize=(12, 5))
+
+ax7.plot(xd2,yd2)
+ax7.plot(xd4,yd4)
+ax7.plot(xd6,yd6)
+ax7.plot(xd8,yd8)
+ax7.plot(xd10,yd10)
+ax7.plot(xd12,yd12)
+ax7.set_title("Evolution du temps moyen d'execution")
+ax7.set_xlabel("Dimension")
+ax7.set_ylabel("time (s)")
+
+ax8.plot(xs2,zs2)
+ax8.plot(xs4,zs4)
+ax8.plot(xs6,zs6)
+ax8.plot(xs8,zs8)
+ax8.plot(xs10,zs10)
+ax8.plot(xs12,zs12)
+ax8.set_title("Evolution de la rapidité de calcul (Gflop/S)")
+ax8.set_xlabel("Dimension")
+ax8.set_ylabel("Gflops/s")
 plt.legend()
 plt.tight_layout()
 plt.show()
